519bb59886646973bf20b905c7ddf6149ead275b
cmolea <cmolea@unitedmotionideas.com>
2014-04-30 15:46:39 +0200

    Disable the Express views system.

--- app.js
+++ app.js
@@ -5,14 +5,7 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var routes = require('./routes/index');
-var users = require('./routes/users');
-
 var app = express();
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
 
 app.use(favicon());
 app.use(logger('dev'));
@@ -21,7 +14,7 @@ app.use(bodyParser.urlencoded());
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
+var users = require('./routes/users');
 app.use('/users', users);
 
 /// catch 404 and forwarding to error handler
@@ -37,22 +30,14 @@ app.use(function(req, res, next) {
 // will print stacktrace
 if (app.get('env') === 'development') {
     app.use(function(err, req, res, next) {
-        res.status(err.status || 500);
-        res.render('error', {
-            message: err.message,
-            error: err
-        });
+        res.send(err.status || 500);
     });
 }
 
 // production error handler
 // no stacktraces leaked to user
 app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-        message: err.message,
-        error: {}
-    });
+    res.send(err.status || 500);
 });
 
 
