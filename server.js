c8041522909bf99702f144e0d64fd6e61d5578f5
cmolea <cmolea@unitedmotionideas.com>
2014-05-06 17:29:20 +0200

    Merge the config file "Application.js" with "server.js"

--- server.js
+++ server.js
@@ -1,13 +1,74 @@
 /**
- * Created by cmolea on 02/05/2014.
+ * ==============================================
+ * Module dependencies.
+ * ==============================================
  */
+var express = require('express'),
+    path = require('path'),
+    logger = require('morgan'),
+    cookieParser = require('cookie-parser'),
+    bodyParser = require('body-parser'),
+    methodOverride = require('method-override'),
+    http = require('http'),
+    mongoose = require('mongoose'),
+    expressValidator = require('express-validator');
 
-var debug = require('debug')('http');
-var app = require('./server/configs/Application');
+/**
+ * ==============================================
+ * Initializing system variables.
+ * ==============================================
+ */
+var config = require('./server/configs/Database');
+mongoose.connect(config.url);
 
+/**
+ * ==============================================
+ * Initializing application.
+ * ==============================================
+ */
+var app = express();
+
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded());
+app.use(expressValidator());
+app.use(cookieParser());
+app.use(methodOverride());
+app.use(express.static(path.join(__dirname, 'public')));
+
+/**
+ * ==============================================
+ * Initializing models and routes.
+ * ==============================================
+ */
+var router = express.Router();
+
+require('./server/modules/feature/FeatureRoutes')(router);
+
+app.use('/api', router);
+
+/**
+ * ==============================================
+ * Error handlers.
+ * ==============================================
+ */
+app.use(function(req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+app.use(function(err, req, res, next) {
+    res.send(err.status || 500);
+});
+
+/**
+ * ==============================================
+ * Start application.
+ * ==============================================
+ */
 app.set('port', process.env.PORT || 3000);
 
 var server = app.listen(app.get('port'), function() {
-    debug('Express server listening on port ' + server.address().port);
     console.log('Express server listening on port ' + server.address().port);
 });
